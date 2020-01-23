10dc360b28ce15867bd362a3127a8a1e3265d425
Riza Khan <khanriza@gmail.com>
2020-01-23 13:20:06 -0500

    Sign up script initalized

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-import { Selector } from 'testcafe';
+import { Selector } from 'testcafe'
 
+fixture `Broadstreet Login`
+  .page `http://ondeck.broadstreetads.com/login`
+
+test('Sign Up Test', async t => {
+
+  const signupButton = await Selector('.single-sign-on').find('a')
+
+  await t
+    .click(signupButton)
+    .typeText('#user_name', 'Tester')
+    .typeText('#user_email', 'test@gtest.com')
+    .typeText('#user_password', 'Testing123!')
+    .click('#human_valid')
+    .click('.pull-right')
+})
